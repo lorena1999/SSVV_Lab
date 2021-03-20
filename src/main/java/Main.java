c958a95ae7825b2d82b73156412d5f4f5fb66cbe
Lorena <lorena.marginean@yahoo.com>
2021-03-20 17:30:21 +0200

    Initial version

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import console.*;
+import domain.*;
+import repository.*;
+import service.*;
+import validation.*;
+
+public class Main {
+
+    static void test(Service service)
+    {
+        service.saveStudent("101", "Student_1", 189);
+        service.saveTema("202", "Assignment_1", 5,2);
+        service.saveNota("101", "202", 8.5, 2, "Good");
+    }
+
+    public static void main(String[] args) {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        UI consola = new UI(service);
+
+        test(service);
+
+        //consola.run();
+
+        //PENTRU GUI
+        // de avut un check: daca profesorul introduce sau nu saptamana la timp
+        // daca se introduce nota la timp, se preia saptamana din sistem
+        // altfel, se introduce de la tastatura
+    }
+}
